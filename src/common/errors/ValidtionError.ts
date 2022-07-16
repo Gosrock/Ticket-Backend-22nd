003058d3eed8fae344a6c0c18fc7cf697112e1d9
이찬진 <water0641@naver.com>
2022-07-16 21:18:07 +0900

    :rocket: feat(error) : validation custom error 추가

---
+++ src/common/errors/ValidtionError.ts
@@ -0,0 +1,32 @@
+import { HttpException, HttpStatus, ValidationError } from '@nestjs/common';
+
+export class CustomValidationError extends HttpException {
+  constructor(valdationErrorArray: ValidationError[]) {
+    const arrayOfError = valdationErrorArray
+      .map((error: ValidationError) => {
+        const constrains = error.constraints;
+        if (!constrains) return null;
+
+        const constrainsErrorStrings = Object.keys(constrains).map(
+          key => constrains[key]
+        );
+        return { [error.property]: constrainsErrorStrings };
+      })
+      .filter(e => e)
+      .reduce(function (result, item) {
+        // 중복값 없다고 가정 object 머지
+        if (!item) return result;
+        if (!result) return result;
+        Object.assign(result, item);
+        return result;
+      }, {}); // null 값 있을경우 필터링
+    super(
+      {
+        error: 'Validation Error',
+        message: arrayOfError,
+        statusCode: HttpStatus.BAD_REQUEST
+      },
+      HttpStatus.BAD_REQUEST
+    );
+  }
+}
