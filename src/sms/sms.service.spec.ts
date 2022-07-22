b0bc4a70ff5160f788a455ece9e165de4387255b
이찬진 <water0641@naver.com>
2022-07-23 02:11:31 +0900

    :hammer: fix(test) : 테스트 spec 에서 config모듈 커스텀으로 변경

--- src/sms/sms.service.spec.ts
+++ src/sms/sms.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CustomConfigModule } from 'src/config/customConfig.module';
 import { SmsService } from './sms.service';
 
 describe('SmsService', () => {
@@ -6,7 +9,21 @@ describe('SmsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SmsService],
+      imports: [
+        CustomConfigModule,
+        HttpModule.registerAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            baseURL: 'https://sens.apigw.ntruss.com/sms/v2/services/',
+            headers: {
+              'Content-type': 'application/json; charset=utf-8',
+              'x-ncp-iam-access-key': '' + configService.get('NAVER_ACCESS_KEY')
+            }
+          }),
+          inject: [ConfigService]
+        })
+      ],
+      providers: [SmsService]
     }).compile();
 
     service = module.get<SmsService>(SmsService);
