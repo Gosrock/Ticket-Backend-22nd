0d9dc8b30a43c7feb02798c139be8c403b91a1ee
gengminy <kls1998@naver.com>
2022-07-26 01:26:24 +0900

    :hammer: fix(socket): SocketService emitToAll 메서드 추가 #51

--- src/tickets/dtos/update-ticket-status.dto.ts
+++ src/tickets/dtos/update-ticket-status.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
-import { IsEnum, IsNumber, IsString } from 'class-validator';
+import { IsEnum, IsNumber } from 'class-validator';
 import { TicketStatus } from 'src/common/consts/enum';
 
 export class UpdateTicketStatusDto {
