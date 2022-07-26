e831755b6c219d9aa5387b7a71e3d47ede00aa9a
이찬진 <water0641@naver.com>
2022-07-26 20:25:26 +0900

    :hammer: fix(order) : any[] 타입 추가, 린트때문에 안돌아감

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException
+} from '@nestjs/common';
 import { RequestOrderDto } from 'src/orders/dtos/request-order.dto';
 import { Order } from 'src/database/entities/order.entity';
 import { User } from 'src/database/entities/user.entity';
@@ -13,100 +17,106 @@ import e from 'express';
 
 @Injectable()
 export class OrdersService {
-	constructor(
-		private orderRepository: OrderRepository,
-		private ticketService: TicketsService,
-		private dataSource: DataSource
-	) {}
+  constructor(
+    private orderRepository: OrderRepository,
+    private ticketService: TicketsService,
+    private dataSource: DataSource
+  ) {}
 
+  // 가격 책정 함수 : 단지 가독성을 위해 함수로 뺌
+  getTotalPrice(requestOrderDto: RequestOrderDto): number {
+    const { selection, ticketCount } = requestOrderDto;
+    const pricePerTicket = 3000; // 티켓 한 장 가격
+    const discountForBoth = 1000; // 양일권에 대한 할인 가격
 
-	// 가격 책정 함수 : 단지 가독성을 위해 함수로 뺌
-	getTotalPrice(requestOrderDto: RequestOrderDto): number {
-		const {selection, ticketCount} = requestOrderDto;
-		const pricePerTicket = 3000;   // 티켓 한 장 가격 
-		const discountForBoth = 1000;  // 양일권에 대한 할인 가격
+    let totalPrice = pricePerTicket * ticketCount;
 
-		let totalPrice = pricePerTicket * ticketCount;
+    if (selection === OrderDate.BOTH) {
+      // ticketCount = 양일권 개수
+      totalPrice = totalPrice * 2 - discountForBoth * ticketCount;
+    }
+    return totalPrice;
+  }
 
-		if (selection === OrderDate.BOTH) {
-			// ticketCount = 양일권 개수
-			totalPrice = (totalPrice * 2) - (discountForBoth * ticketCount);
-		}
-		return totalPrice;
-	}
+  /**
+   * selection과 ticketCount를 요청받아서 가격 책정 후 주문을 생성하고,
+   * 해당 주문에 포함되는 모든 티켓을 각각 생성한다.
+   * @param requestOrderDto {selection, ticketCount}
+   * @param user Request User
+   * @returns Order
+   */
+  async createOrder(
+    requestOrderDto: RequestOrderDto,
+    user: User
+  ): Promise<Order> {
+    const { selection, ticketCount } = requestOrderDto;
+    const totalPrice = this.getTotalPrice(requestOrderDto); // 가격 책정
 
-	/**
-	 * selection과 ticketCount를 요청받아서 가격 책정 후 주문을 생성하고,
-	 * 해당 주문에 포함되는 모든 티켓을 각각 생성한다.
-	 * @param requestOrderDto {selection, ticketCount}
-	 * @param user Request User
-	 * @returns Order
-	 */
-	async createOrder(requestOrderDto: RequestOrderDto, user: User): Promise <Order> {
-		const {selection, ticketCount} = requestOrderDto;
-		const totalPrice = this.getTotalPrice(requestOrderDto); // 가격 책정
+    // 양일권일 경우 요청수량 * 2 해서 저장
+    if (selection === OrderDate.BOTH) {
+      requestOrderDto.ticketCount *= 2;
+    }
 
-		// 양일권일 경우 요청수량 * 2 해서 저장
-		if (selection === OrderDate.BOTH) {
-			requestOrderDto.ticketCount *= 2;
-		}
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-		const queryRunner = this.dataSource.createQueryRunner();
-		await queryRunner.connect();
-		await queryRunner.startTransaction();
+    const connectedOrder = getConnectedRepository(
+      OrderRepository,
+      queryRunner,
+      Order
+    );
+    const connectedTicket = getConnectedRepository(
+      TicketRepository,
+      queryRunner,
+      Ticket
+    );
 
-		const connectedOrder = getConnectedRepository(
-			OrderRepository,
-			queryRunner,
-			Order
-		);
-		const connectedTicket = getConnectedRepository(
-			TicketRepository,
-			queryRunner,
-			Ticket
-		);
+    try {
+      // 주문 생성
+      const order = await connectedOrder.createOrder(
+        requestOrderDto,
+        user,
+        totalPrice
+      );
 
-		try {
-			// 주문 생성
-			const order = await connectedOrder.createOrder(requestOrderDto, user, totalPrice);
+      // ticketList에 생성할 티켓을 모두 담은 후 한번에 비동기요청
+      const ticketList: any[] = [];
 
-			// ticketList에 생성할 티켓을 모두 담은 후 한번에 비동기요청
-			const ticketList = new Array;
+      for (let i = 0; i < ticketCount; i++) {
+        if (selection === OrderDate.BOTH || selection === OrderDate.YB) {
+          const createTicketDto = {
+            date: PerformanceDate.YB,
+            order,
+            user
+          };
+          ticketList.push(createTicketDto);
+        }
+        if (selection === OrderDate.BOTH || selection === OrderDate.OB) {
+          const createTicketDto = {
+            date: PerformanceDate.OB,
+            order,
+            user
+          };
+          ticketList.push(createTicketDto);
+        }
+      }
+      await Promise.all(
+        ticketList.map(dto => connectedTicket.createTicket(dto))
+      );
 
-			for (let i = 0; i < ticketCount; i++) {
-				if (selection === OrderDate.BOTH || selection === OrderDate.YB) {
-					const createTicketDto = {
-						date: PerformanceDate.YB,
-						order,
-						user
-					}
-					ticketList.push(createTicketDto);
-				}
-				if (selection === OrderDate.BOTH || selection === OrderDate.OB) {
-					const createTicketDto = {
-						date: PerformanceDate.OB,
-						order,
-						user
-					}
-					ticketList.push(createTicketDto);
-				}
-			}
-			await Promise.all(ticketList.map((dto) => connectedTicket.createTicket(dto)));
+      await queryRunner.commitTransaction();
+      return order;
+    } catch (e) {
+      // 주문 생성 실패시 Error
+      await queryRunner.rollbackTransaction();
+      throw e;
+    } finally {
+      await queryRunner.release();
+    }
+  }
 
-			await queryRunner.commitTransaction();
-			return order;
-
-		} catch (e) {
-			// 주문 생성 실패시 Error
-			await queryRunner.rollbackTransaction();
-			throw e;
-			
-		} finally {
-			await queryRunner.release();
-		}	
-	}
-
-	async findAllByUserId(userId: number): Promise <Order[]> {
-		return await this.orderRepository.findAllByUserId(userId);
-	}
+  async findAllByUserId(userId: number): Promise<Order[]> {
+    return await this.orderRepository.findAllByUserId(userId);
+  }
 }
